806ccc6e36e0a4243a2af3cb9c4ebd0003a0eb40
Arun Narayanaperumal Chellathurai <arun5@illinois.edu>
2021-12-09 17:16:15 -0600

    updated codescan fixes

--- pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/ASTParseErrorTest.java
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/ASTParseErrorTest.java
@@ -1,17 +1,26 @@
 package net.sourceforge.pmd.lang.plsql.ast;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import net.sourceforge.pmd.lang.plsql.AbstractPLSQLParserTst;
 
+/**
+ * Test to replciate the SQL parser error
+ */
+
 public class ASTParseErrorTest extends AbstractPLSQLParserTst {
 
+    /**
+     * Test method to execute parse based on the test SQL from file
+     */
     @Test
-    public void ASTParserError() {
+    public void astParserError() {
         try {
             ASTInput input = plsql.parseResource("ASTErrorParse.sql");
+            Assert.assertNotNull("Null, No good",input);
         } catch (Exception ex) {
-            assert (ex.getMessage().contains("SELECT"));
+            Assert.assertTrue ("No Match No Good",ex.getMessage().contains("SELECT"));
         }
     }
 }
